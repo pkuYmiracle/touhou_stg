230f983dce9339cd99b27c563e423798429e9c4e
pkuYmiracle <1771235420@qq.com>
2022-05-19 10:59:34 +0000

    update level_menu.cpp.

--- level_menu.cpp
+++ level_menu.cpp
@@ -69,8 +69,16 @@ void Level_menu::turn_left()
 
     pos = (pos + characters.size() - 1) %  characters.size();
     delete character_postion;
+
     character_postion = new Mypushbottom(this,true,
                                                   characters[pos],150);
+    connect(character_postion,&Mypushbottom :: clicked,[&](){
+        QTimer::singleShot(300,[&](){
+             Game_board * game = new Game_board(this,id,characters[pos]);
+             game ->show();
+             this->hide();
+        });
+    });
     character_postion->resize(500,600);
     character_postion->move(100 + 50 ,this->height()/2 - character_postion->height()/2);
     character_postion->show();
@@ -82,6 +90,13 @@ void Level_menu::turn_right()
     delete character_postion;
     character_postion = new Mypushbottom(this,true,
                                                   characters[pos],150);
+    connect(character_postion,&Mypushbottom :: clicked,[&](){
+        QTimer::singleShot(300,[&](){
+             Game_board * game = new Game_board(this,id,characters[pos]);
+             game ->show();
+             this->hide();
+        });
+    });
     character_postion->resize(500,600);
     character_postion->move(100 + 50 ,this->height()/2 - character_postion->height()/2);
     character_postion->show();
